3b41f64fcee51ccfc3c5b55682f7777b142545d6
Benjamin <bsadickinson@gmail.com>
2022-08-01 22:28:48 +0000

    completed reverse string

--- projects/scratch-pad/day-1/homework/reverse-string.js
+++ projects/scratch-pad/day-1/homework/reverse-string.js
@@ -19,12 +19,26 @@
  *  d. how do you concatenate Strings? What operator do we use?
  */
 
+//I: a string
+//O: a new string made from the old string spelled backwards
+//C:
+//E:
+
 function reverseString(input) {
     // YOUR CODE GOES BELOW HERE //
-    
-    
-    
-    
+    //create array to hold the values from the string
+    var forwardArray = input.split("");
+    //create an array to hold the values in the reversed order
+    var backwardArray = [];
+    //create a variable to access indexes in the backward array
+    var backCounter = 0;
+    //loop backwards over the foward array to fill the backwards array.
+    for(var index = forwardArray.length - 1; index >=0; index--){
+      backwardArray[backCounter] = forwardArray[index];
+      backCounter++;
+    }
+    return backwardArray.join("");
+  
     // YOUR CODE GOES ABOVE HERE //
 }
 
