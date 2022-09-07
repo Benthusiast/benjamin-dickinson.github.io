0246b26d9ab3ad1c3acb0c9b8c4f4a74ee817136
Benjamin <bsadickinson@gmail.com>
2022-09-07 02:01:40 +0000

    studies project operators in progress

---
+++ studies/operators.js
@@ -0,0 +1,81 @@
+/**
+ * OPERATORS
+ * 
+ * Operators perform actions, or operations, on values in Javascript.
+ * Using operators to build expressions, we can create and change values
+ * in memory.
+ *   
+ * There are many types of Operators, organized by their general use.
+ * 
+ */
+
+/**
+ * Assignment Operators like = and += assign or reassign variables to given or resulting values.
+ * 
+ * examples include 
+ */
+// =   
+var myName = "Ben"; //assigns the variable myName the value of the string "Ben"
+var myInt = 74;
+var bDay = 53;
+
+
+// +=
+myName += "jamin"; // adds the string "Ben" and "jamin" then ASSIGNS the result, "Benjamin", to myName
+myInt += 1; //the value of myInt is reassigned to the result of 74 + 1 => 75
+
+// -=
+myInt -=1 //the value of myInt is reassigned to the result of 75 - 1 => 74
+
+// *=
+myInt *=2 //the value of myInt is reassigned to the result of 74 * 2 => 148
+
+// /=
+myInt /= 2 //the value of myInt is reassigned to the result of 148 / 2 => 74
+
+
+/**
+ * Arithmetic Operators perform mathematical operations on numerical values to return a new value.
+ * 
+ * examples include
+ */
+
+// +
+myInt + bDay; //add the values together and returns the result => 127
+
+// -
+myInt - bDay; //subracts the scond value from the first and return the result => 21
+
+// *
+myInt * bDay; //returns the product of the two values => 3922
+
+// /
+myInt / bDay; //returns the quotient of the two values => 1.39622641509434...
+
+// %
+myInt % bDay; //returns the remainder after divinding the two values => 21
+
+// **
+myInt ** 2; //returns the first number rasied to the power of the second number
+
+// ++
+myInt++; //Increments the value by 1, equivalent to += 1 
+
+// --
+myInt-- //Decrements the value by 1, equivalent to -= 1
+
+/**
+ * Comparison Operators
+ */
+
+/**
+ * Logical
+ */
+
+/**
+ * Unary
+ */
+
+/**
+ * Ternary Operators
+ */
--- studies/variables.js
+++ studies/variables.js
@@ -1,23 +1,34 @@
 /*
  * VARIABLES:
  *
- * 0. Variables are named identifiers that can point to values of a particular type, like a Number, 
- * String, Boolean, Array, Object or another data-type.  Variables are called so because once created, 
- * we can CHANGE the value (and type of value) to which they point.
+ * 0.   Variables are point to values of a particular type, like a Number, String, Boolean, Array, Object 
+ *      or another data-type.  Variables are called so because once created, 
+ *      we can CHANGE the value (and type of value) to which they point.
  *
- * 1. To create a variable we use the keywords var, let or const followed by a name which is ususally a 
- *string and is formatted in camel case.
+ * 1.   To create a variable we use the keywords var, let or const followed by a name which is ususally a 
+ *      string and is formatted in camel case.
+ * 
+ *      1. var - this is the original variable keyword in javascript. Variables declared with the var 
+ *      keyword are hoisted to the top of their scope, and are reassignable.
+ * 
+ *      2. let - Variables declared with the let keyword are not hoised to the top of their scope, but
+ *      are instead block scoped, meaning they are inaccessible in higher scopes than they are created.
+ * 
+ *      3. const - Variables declared with const are block scoped as is let, but unlike let, a variable
+ *      declared with const cannot be reassigned.  This is often used with collections such as arrays to
+ *      allow for the editing of the values IN the array, but not the reassignment of the array variable.
  *
- * 2. There are 2 steps to using variables: declaration and assignment/initialization.
+ * 
+ * 2.   There are 2 steps to using variables: declaration and assignment/initialization.
  */
 
 // 1. declaration //
 var myName;
 let yourName;
 
-/*
- * At the declaration phase, the variable myName is undefined because we have NOT initialized
- * it to anything
+/**
+ * At declaration, the variable myName is undefined because it has not
+ * been assigned a value
  */
 console.log(myName); // prints => undefined
 
@@ -29,7 +40,7 @@ console.log(myName); // prints => Benjamin
 const namesArray = ["Ben", "Brittany", "Geovanni"];
 console.log(namesArray); // prints => ["Ben", "Brittany", "Geovanni"]
 
-// 3. re-assignment //
+// 2a. re-assignment //
 myName = 'Benjamin';
 console.log(myName); // prints => Benjamin
 
@@ -37,3 +48,17 @@ console.log(myName); // prints => Benjamin
 var myVariable = 1;
 var myVariable = true;
 myVariable = "someString";
+
+
+/**
+ * HOISTING
+ * 
+ * Hoisting is a behavior in Javascript that allows variables to be accessed before they are actually declared.
+ * This is because variable declarations that use the var keyword and function declarations are "moved" to the 
+ * top of the scope when the code is run. 
+ * The best example of this is with the var keyword.
+ */
+
+console.log(hoistME); /* because the declaration of the variable, BUT NOT ITS ASSIGNMENT, is hoisted, this
+                         will print => undefined */ 
+var hoistME = "Please!";
